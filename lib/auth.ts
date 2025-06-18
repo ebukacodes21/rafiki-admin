697aede49ac03d8a3112be1df653667649c1ac96
ebukacodes21 <georgeokafo1@gmail.com>
2025-06-18 01:15:14 +0100

    auth component fixed

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,26 +1,23 @@
-"use server";
-
-import { V2 } from "paseto";
-import { cookies } from "next/headers";
-import { COOKIE_NAME } from "@/constants";
+import {V2} from 'paseto';
+import { cookies } from 'next/headers';
+import { COOKIE_NAME } from '@/constants';
 
 const PUBLIC_KEY = process.env.PASETO_PUBLIC_KEY!;
+console.log(PUBLIC_KEY, "key");
 
 export async function getUserFromToken() {
   const cookieStore = cookies();
   const token = (await cookieStore).get(COOKIE_NAME)?.value;
-console.log(token)
+  console.log(token, "here");
   if (!token) return null;
 
   try {
-    const payload = await V2.verify(token, PUBLIC_KEY, {
-      audience: "rafiki-admin",
-      issuer: "rafiki",
-    });
-
-    return payload; 
+    const key = Buffer.from(PUBLIC_KEY, 'base64'); 
+    const payload = await V2.verify(token, key);
+    console.log(payload);
+    return payload;
   } catch (err) {
-    console.error("Invalid token:", err);
+    console.error('Invalid token:', err);
     return null;
   }
 }
