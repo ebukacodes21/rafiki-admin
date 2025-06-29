06f90f04d88a5a5868c821bb5a058d927ab08fc5
ebukacodes21 <georgeokafo1@gmail.com>
2025-06-29 17:56:55 +0100

    latest fix

--- app/(routes)/settings/page.tsx
+++ app/(routes)/settings/page.tsx
@@ -1,10 +1,16 @@
-"use client"
-import React from 'react'
-import SettingsClient from './components/client'
-import { useRequireAuth } from '../hooks/useAuth'
+"use client";
+import React from "react";
+import SettingsClient from "./components/client";
+import { useRequireAuth } from "../hooks/useAuth";
+import { ScaleIcon } from "@heroicons/react/24/solid";
 
 const SettingsPage = () => {
-  useRequireAuth()
+  const loading = useRequireAuth();
+
+  if (loading) {
+    return <ScaleIcon className="h-5 w-5" />;
+  }
+  
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
--- app/api/connect-stripe/route.ts
+++ app/api/connect-stripe/route.ts
@@ -4,7 +4,7 @@ import ApiConfig from "@/services/api-config";
 import { COOKIE_NAME, routes } from "@/constants";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2025-05-28.basil",
 });
 
